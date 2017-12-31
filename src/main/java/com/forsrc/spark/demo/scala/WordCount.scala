5d5c4581ee635faa97a4c231aeea9b298cb1ca54
forsrc <forsrc@gmail.com>
2017-12-31 11:10:21 +0800

    scala

--- src/main/java/com/forsrc/spark/demo/scala/WordCount.scala
+++ src/main/java/com/forsrc/spark/demo/scala/WordCount.scala
@@ -6,11 +6,12 @@ import org.apache.spark.SparkConf
 object WordCount {
 
     def main(args: Array[String]): Unit = {
-        val inputFile = "README.md"
+        val inputFile = WordCount.getClass.getClassLoader.getResource("WordCount.txt").getFile 
         val conf = new SparkConf().setAppName("WordCount").setMaster("local")
         val sc = new SparkContext(conf)
         val textFile = sc.textFile(inputFile)
         val wordCount = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
         wordCount.foreach(println)
+        sc.stop();
     }
 }
